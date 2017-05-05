1fdfc9df5ef92cf6d8f057205ac12be1f37d0f9d
lilins <lule0102@gmail.com>
2017-05-05 23:03:33 +0800

    update: 时间

--- getrss.js
+++ getrss.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 var rss = require("./models/rss.js");
 //定时器任务，每分钟的第30秒开始运行脚本，获取所有的feed列表中的信息。
 var rule     = new schedule.RecurrenceRule();  
-var times    = [0,30];  
+var times    = [1,30];  
 rule.minute  = times;  
 var j = schedule.scheduleJob(rule, function(){
   rss.start();
